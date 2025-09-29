9dd077081222ad8481e07709562b535c5242eb65
Alexandros Fitianidis <fitianidis.alex@gmail.com>
2025-09-30 00:32:23 +0300

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+
+package Calculator;
+
+/**
+ *
+ * @author alexfitia
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello eeorld!");
+    }
+}
